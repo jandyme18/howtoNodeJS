8a0313747077a14460c50eb2e736604d6bc44a41
jandyme18 <natwalan.pumpuang@gmail.com>
2024-06-18 19:21:28 +0700

    Update app.js
    Make products route from express router

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const productsRouter = express.Router();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -12,6 +13,13 @@ app.use(express.static(path.join(__dirname, '/public/')))
 
 app.set("views", "./src/views")
 app.set("view engine", "ejs")
+
+productsRouter.route("/").get((req, res) => {
+    res.send("Products are here")
+});
+
+
+app.use("/products", productsRouter)
 
 app.get("/", (req,res) => {
     res.render('index',{username: 'jandyme18', customers: ["Catareen","Tasia","Frongz"]});
