1c1cae4ce59370b2f37fa6069d8ccf1066f2a9e4
jandyme18 <natwalan.pumpuang@gmail.com>
2024-06-18 16:52:06 +0700

    First Commit
    Change port number in console.log color from green to red

--- app.js
+++ app.js
@@ -12,5 +12,5 @@ app.get("/", (req,res) => {
 })
 
 app.listen(port, () => {
-    debug("listening on port" + chalk.green(": " + port));
+    debug("listening on port" + chalk.red(": " + port));
 })
