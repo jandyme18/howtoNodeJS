f3d730a9cdbef24671666d91a87a302711c74ff4
jandyme18 <natwalan.pumpuang@gmail.com>
2024-06-18 16:31:03 +0700

    Add all file of project
    move all file to Github repo

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
+
+app.use(morgan('combined')); //ไว้ดู log รายละเอียดว่าเข้าจากเครื่องไหน
+
+app.get("/", (req,res) => {
+    res.send('Hello, Jandy Nat!');
+})
+
+app.listen(port, () => {
+    debug("listening on port" + chalk.green(": " + port));
+})
