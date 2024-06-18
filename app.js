9296c6cebfd309b6510b77652e148bca16e436cf
jandyme18 <natwalan.pumpuang@gmail.com>
2024-06-18 17:42:47 +0700

    second commit
    1. use static path in public folder
    
    2. add sample html template from website One Page Love

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
+
 const app = express();
 const port = 3000;
 
 app.use(morgan('combined')); //ไว้ดู log รายละเอียดว่าเข้าจากเครื่องไหน
+app.use(express.static(path.join(__dirname, '/public/')))
 
 app.get("/", (req,res) => {
     res.send('Hello, Jandy Nat!');
