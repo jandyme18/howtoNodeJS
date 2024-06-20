8b8042833ca2b0d35f3f461c68fafe93de04acf3
jandyme18 <natwalan.pumpuang@gmail.com>
2024-06-21 01:01:41 +0700

    Update app.js
    fix syntax

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ app.set("views", "./src/views")
 app.set("view engine", "ejs")
 
 productsRouter.route("/").get((req, res) => {
-    res.render("products",{
+    res.render("products",
         products,
-    });
+    );
 });
 
 productsRouter.route("/").get((req, res) => {
