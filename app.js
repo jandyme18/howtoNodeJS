0a8ebe598c1e47dbad2ec175ee5193226f92c6b8
jandyme18 <natwalan.pumpuang@gmail.com>
2024-06-21 00:50:59 +0700

    update app.js
    fix syntax

--- app.js
+++ app.js
@@ -15,14 +15,14 @@ app.set("views", "./src/views")
 app.set("view engine", "ejs")
 
 productsRouter.route("/").get((req, res) => {
-    res.render("products"),{
+    res.render("products",{
         products: [
             {productTitle: 'Dish soap', productDescription: 'Formulation 1 Super Clean Dish Soap', productPrice: '45'},
             {productTitle: 'Dish soap 2', productDescription: 'Formulation 2 Super Clean Dish Soap', productPrice: '65'},
             {productTitle: 'Dish soap 3', productDescription: 'Formulation 3 Super Clean Dish Soap', productPrice: '35'},
             {productTitle: 'Dish soap 4', productDescription: 'Formulation 4 Super Clean Dish Soap', productPrice: '55'}
         ],
-    }
+    });
 });
 
 productsRouter.route("/").get((req, res) => {
