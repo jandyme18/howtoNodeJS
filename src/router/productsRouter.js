728b3275f9204e576f96e675c705edd209455da6
jandyme18 <natwalan.pumpuang@gmail.com>
2024-06-21 22:09:05 +0700

    Update productsRouter.js
    change method to get products id

--- src/router/productsRouter.js
+++ src/router/productsRouter.js
@@ -3,17 +3,17 @@ const productsRouter = express.Router();
 const products = require('../data/products.json');
 
 productsRouter.route("/").get((req, res) => {
-    res.render("products",{
-        products,
-    }
-    );
+    res.render("products", { products });
 });
 
 productsRouter.route("/:id").get((req, res) => {
     const id = req.params.id;
-    res.render("product",{
-        product: products[id],
-    });
+    const product = products.find(p => p.id === id);
+    if (product) {
+        res.render("product", { product });
+    } else {
+        res.status(404).send('Product not found');
+    }
 });
 
 module.exports = productsRouter;
